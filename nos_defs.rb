36b292310eb892fa0d839e72976cec1adcb609d5
Dinesh G Dutt <dd.ps4u@gmail.com>
2020-10-10 17:36:27 -0700

    Remove the specifc instance defs and such from Vagrantfile

---
+++ nos_defs.rb
@@ -0,0 +1,15 @@
+NOS_MEM = {
+  "cumulus" => 768,
+  "nxos" => 6144,
+  "junos" => 2048,
+  "eos" => 2048
+}
+
+NOS_BOX = {
+  "cumulus" => "CumulusCommunity/cumulus-vx",
+  "nxos" => "cisco/nxosv",
+  "junos" => "juniper/vqfx10k-re",
+  "eos" => "arista/veos"
+}
+
+SERVER_BOX = "generic/ubuntu1804"
