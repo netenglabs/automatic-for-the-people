619d588fafee12fe03357f7f911082af9ff335a6
Dinesh G Dutt <dd.ps4u@gmail.com>
2021-04-11 09:52:04 -0700

    Various updates and fixes

--- 2t-clos-dual-attach/nos_defs.rb
+++ 2t-clos-dual-attach/nos_defs.rb
@@ -2,14 +2,16 @@ NOS_MEM = {
   "cumulus" => 768,
   "nxos" => 6144,
   "junos" => 2048,
-  "eos" => 2048
+  "eos" => 2048,
+  "iosxr" => 7168
 }
 
 NOS_BOX = {
   "cumulus" => "CumulusCommunity/cumulus-vx",
   "nxos" => "cisco/nxosv",
   "junos" => "juniper/vqfx10k-re",
-  "eos" => "arista/veos"
+  "eos" => "arista/veos",
+  "iosxr" => "cisco/iosxrv9k",
 }
 
 NOS_VERSION = {
@@ -17,6 +19,7 @@ NOS_VERSION = {
   "nxos" => "0",
   "eos" => "4.23.5M",
   "junos" => "0.4.0",
+  "iosxr" => "6.6.2",
 }
 
 SERVER_BOX = "generic/ubuntu1804"
